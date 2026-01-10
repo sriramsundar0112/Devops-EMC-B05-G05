1b38d1dc146401c9f5ab34b00630f3ec212e620a
sriramsundar0112 <devops2t25@gmail.com>
2026-01-10 12:59:23 +0530

    Modifying login page and about page to suit the website (#10)

--- js/script.js
+++ js/script.js
@@ -2,14 +2,32 @@
 console.log("About page loaded");
 
 // Show welcome alert when page loads
-window.onload = function() {
-    alert("Welcome to our About page!");
-}
+// window.onload = function() {
+//     alert("Welcome to our About page!");
+// }
 
 // Change color of headings when clicked
+
 const headings = document.querySelectorAll("h2, h3");
 headings.forEach(h => {
     h.addEventListener("click", () => {
         h.style.color = "#e67e22"; // Change color on click
     });
 });
+
+function showWelcomeMessage() {
+            // Get value entered in username field
+            const username = document.getElementById("username").value;
+
+            // Check if username is empty
+            if (username === "") {
+                alert("Please enter your username");
+                return false;
+            }
+
+            // Show welcome popup
+            alert("Welcome " + username);
+
+            // Prevent actual form submission
+            return false;
+        }
